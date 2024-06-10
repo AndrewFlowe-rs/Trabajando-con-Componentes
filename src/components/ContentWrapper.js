47d1cb230046807106732f86791004f195b5b35c
AndrewFlowe-rs <arnaldomahone@gmail.com>
2024-06-09 22:15:08 -0300

    se termina el desafio trabajando con componentes

---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,74 @@
+import React from "react";
+import TopBar from "./TopBar";
+import ContentRowTop from "./ContentRowTop";
+import Footer from "./Footer";
+import { TableHead } from "./TableHead";
+import { TableRow } from "./TableRow";
+function ContentWrapper() {
+  const metrics = [
+    {
+      show: true,
+      title: "Movies in Data base ",
+      digit: 21,
+      color: "primary",
+      icon: "film",
+    },
+    {
+      show: true,
+      title: "Total awards",
+      digit: 79,
+      color: "success",
+      icon: "award",
+    },
+    {
+      show: true,
+      title: "Actors quantity",
+      digit: 49,
+      color: "warning",
+      icon: "users",
+    },
+  ];
+  const movies = [
+    {
+      title: "Billie Eliot",
+      duration: "123",
+      rating: 5,
+      genre: ["Comedy", "Drama"],
+      awards: 2,
+    },
+    {
+      title: "Alicia en el país de las maravillas",
+      duration: "142",
+      rating: 4.8,
+      genre: ["Drama", "Acción", "Comedia"],
+      awards: 3,
+    },
+  ];
+
+  const dataTable = ["Título", "Duración", "Rating", "Géneros", "Premios"];
+  return (
+    <React.Fragment>
+      {/*<!-- Content Wrapper -->*/}
+      <div id="content-wrapper" className="d-flex flex-column">
+        {/*<!-- Main Content -->*/}
+        <div id="content">
+          <TopBar />
+          <ContentRowTop data={metrics} />
+          <table class="table table-bordered">
+            <TableHead items={dataTable} />
+            <tbody>
+              {
+              movies.map((movie, i) => {
+                return <TableRow key={i} {...movie} />;
+              })}
+            </tbody>
+            <TableHead items={dataTable} />
+          </table>
+
+          <Footer />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+export default ContentWrapper;
